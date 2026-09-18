5de7d894c66c48ef6ad93b1ad1bd2ffaa7931cba
Anna <annpok098@gmail.com>
2026-09-18 21:36:46 +0300

    about 1440

---
+++ js/main.js
@@ -0,0 +1,4 @@
+const yearElem = document.getElementById("currentYear");
+const currentYear = new Date().getFullYear();
+
+yearElem.innerText = currentYear;
---
+++ js/slider.js
@@ -0,0 +1,9 @@
+const swiper = new Swiper(".teamSwiper", {
+  slidesPerView: 4,
+  spaceBetween: 40,
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
