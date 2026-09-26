c429d012c01b14e56ecc3b2a3031ca05fb8344b1
Anna <annpok098@gmail.com>
2026-09-26 10:51:39 +0300

    fix

--- js/home.js
+++ js/home.js
@@ -5,11 +5,11 @@ const reviewsSlider = new Swiper(".reviews-block__slider", {
   grabCursor: true,
   loop: true,
 
-  // autoplay: {
-  //   delay: 4000, // Слайд гортається кожні 4 секунди
-  //   disableOnInteraction: false, // Автогортання не ламається після ручного кліку
-  //   pauseOnMouseEnter: true, // МАГІЯ ТУТ: зупиняє гортання, коли мишка НАВЕДЕНА на слайдер
-  // },
+  autoplay: {
+    delay: 4000, // Слайд гортається кожні 4 секунди
+    disableOnInteraction: false, // Автогортання не ламається після ручного кліку
+    pauseOnMouseEnter: true, // МАГІЯ ТУТ: зупиняє гортання, коли мишка НАВЕДЕНА на слайдер
+  },
 
   pagination: {
     el: ".reviews-block__pagination",
